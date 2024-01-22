e6b60bf2ff3be0e5d3961cd71fd47d6f5de8c147
LuizCesarLeite <luiz@mentalmarketing.com.br>
2024-01-22 15:38:57 -0300

    Infos backend Strapi sendo chamados na home com sucesso

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const ejs = require('ejs');
 const app = express();
 
@@ -7,9 +8,23 @@ console.log(process.env.TESTE_ENV);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.get('/', function(req, res) {
-    res.render('pages/index');
+app.use(express.json());
+
+// app.get('/', function(req, res) {
+//     res.render('pages/index');
+// });
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://mentalmarketing.com.br/strapi/api/home-page?populate=deep');
+        const data = response.data;
+        res.render('pages/index', { data: data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error ao obter dados');
+    }
 });
+
 app.listen(4682, () => {
     console.log('Rodando suave na porta 4682');
 });
