d70fa078c38853b374a6e7418825c9f5979b8df5
Luiz Cesar Leite <luiz@mentalmarketing.com.br>
2025-04-22 17:17:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 
 require('dotenv').config();
-console.log(process.env.TESTE_ENV);
 
 const PORT = 4682;
 
@@ -19,33 +18,48 @@ requiredEnvVars.forEach(varName => {
     }
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', async (req, res) => {
-    try {
-        const response = await axios.get('https://mentalmarketing.com.br/strapi/api/home-page?populate=deep');
-        const homeData = response.data;
-        res.render('pages/index', { homeData: homeData });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error ao obter dados');
-    }
-});
+const BASE_URL = 'https://mentalmarketing.com.br/strapi';
 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minutos
     max: 100 // limita cada IP em 100 requisições durante 10 minutos
 });
 
-app.use('/send-data', limiter);
 const sendDataRouter = require('./routes/sendData');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/send-data', limiter);
 app.use('/', sendDataRouter);
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/api/home-page?populate=deep`);
+        
+        const homeData = response.data;
+        // console.log('Home Data:', JSON.stringify(homeData, null, 2));
+
+        const logoMental = homeData.data.attributes.logoMental.data[0].attributes.url;
+        // console.log(logoMental);
+
+        const localizations = homeData.data.attributes.localizations.data.map(localization => localization.attributes);
+        // console.log('Localizations:', JSON.stringify(localizations, null, 2));
+
+        res.render('pages/index', { 
+            homeData: homeData, 
+            logoMental: logoMental,
+            localizations: localizations, 
+            BASE_URL: BASE_URL
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error ao obter dados');
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-
