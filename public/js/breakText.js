b854404b459cbaca220899e187970ebf4289684c
Paulo H. <phafonsoesilva@gmail.com>
2025-06-09 17:28:53 -0300

    Removing comments from breakText func

--- public/js/breakText.js
+++ public/js/breakText.js
@@ -3,20 +3,7 @@ const texts = document.getElementsByClassName('breakText')
 const arr = [...texts]
 
 arr.forEach(item => {
-    console.log('AAA', item );
     text = item.innerHTML
     let textArr = text.split('&lt;br&gt;')
     item.innerHTML = `${textArr[0]} <br> ${textArr[1]}`
 })
-
-
-
-// console.log('AAAA', document.getElementsByClassName('breakText')[0].innerHTML); 
-
-// const text = document.getElementsByClassName('breakText')[0].innerHTML
-
-// let textArr = text.split('&lt;br&gt;')
-
-// console.log('BBB', textArr);
-
-// document.getElementsByClassName('breakText')[0].innerHTML = `${textArr[0]} <br> ${textArr[1]} `
