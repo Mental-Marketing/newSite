e5fa2c849d5efa0b212a9a8aa2c7cc121929e293
Paulo H. <phafonsoesilva@gmail.com>
2025-07-04 11:04:16 -0300

    visual adjusts on slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -3,13 +3,10 @@ let arrLogos = {...homeData.data.sliderLogos}
 const BASE_URL = 'https://mentalmarketing.com.br/strapi';
 const sliderDiv = document.getElementsByClassName('multiple-items')[0]
 
-        
 Object.values(arrLogos).map((item, i)=>{
 
     const itemDiv = document.createElement('div')
     itemDiv.setAttribute("class","brand")
-    //itemDiv.setAttribute("id",item.name)
-    //itemDiv.setAttribute("key", item.name)
 
     const itemImg = document.createElement('img')
     itemImg.setAttribute("class","brand_img")
@@ -20,13 +17,12 @@ Object.values(arrLogos).map((item, i)=>{
 })
 
 $('.multiple-items').slick({
-    centerMode: false,
-    centerPadding: '60px',
+    centerMode: true,
+    centerPadding: '10px',
     slidesToScroll: 1,
+    swipeToSlide: true,
     autoplay: true,
     autoplaySpeed: 2000,
-    infinite: true,
-    loop: true,
     arrows: false,
     slidesToShow: 3,
     responsive: [
